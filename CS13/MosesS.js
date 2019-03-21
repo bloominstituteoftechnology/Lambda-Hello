39d05b91cc9b09d4242ba493b7c05e755415039c
MosesSupposes <mosamuel98@gmail.com>
2019-03-21 11:49:55 -0700

    introduce myself

---
+++ CS13/MosesS.js
@@ -0,0 +1,8 @@
+var Moses = {
+    name: this,
+    adjectives: ['adaptable', 'pensive', 'inventive'],
+    favoriteHobby: 'watching anime',
+    favoriteAnime: 'Death Note',
+    secondFavoriteAnime: 'Steins; Gate',
+    currentlyTheCoolestStudentAtLambda: true
+}
