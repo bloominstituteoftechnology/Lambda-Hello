e9770ba3b8f615356b828e5c653287a6e6e3a976
Kerby <kerby.jean@hotmail.fr>
2020-01-13 16:27:50 -0500

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var dict: [Int: String] = [1: "test"]
+
+for dic in dict {
+    print(dic.value)
+}
+
+
+
+var roll = 6
+
+switch roll {
+case 1...6:
+    print("1 to 6")
+default:
+    print("done")
+}
+    
+
+
+
